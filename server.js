9331b348c6c7c528cecac30680b43830bf9a1a4f
Roman Larionov <pcsaveddotcom@gmail.com>
2024-01-02 20:20:12 -0500

    set up initial routes and added index/head/header ejs files

--- server.js
+++ server.js
@@ -21,3 +21,74 @@ mongoose.connection
     console.log("error")
 })
 
+/////////////////////////
+
+// destructure Schema and model into their own variables
+const {Schema, model} = mongoose
+// const Schema = mongoose.Schema
+// const model = mongoose.model
+
+// Schema - Shape of the Data
+const fruitSchema = new Schema({
+    name: String,
+    color: String,
+    readyToEat: Boolean
+})
+
+// Model - object for interacting with the db
+const Fruit = model("Fruit", fruitSchema)
+
+const app = express();
+
+////////////////////////
+
+app.use(morgan("tiny")) //logging
+app.use(methodOverride("_method")) // override for put and delete requests from forms
+app.use(express.urlencoded({extended: true})) // parse urlencoded request bodies
+app.use(express.static("public")) // serve files from public statically
+
+
+////////////////////////////////////////////
+app.get("/", (req, res) => {
+    res.send("your server is running... better catch it.")
+})
+
+////////////////////////////////////////////
+app.get("/fruits/seed", async (req, res) => {
+
+    try{
+        // array of starter fruits
+        const startFruits = [
+          { name: "Orange", color: "orange", readyToEat: false },
+          { name: "Grape", color: "purple", readyToEat: false },
+          { name: "Banana", color: "orange", readyToEat: false },
+          { name: "Strawberry", color: "red", readyToEat: false },
+          { name: "Coconut", color: "brown", readyToEat: false },
+          ]
+       // Delete all fruits
+       await Fruit.deleteMany({})
+       // Seed Starter Fruits
+       const fruits = await Fruit.create(startFruits)
+       // send created fruits as response to confirm creation
+       res.json(fruits);
+    } catch(error) {
+      console.log(error.message)
+      res.status(400).send(error.message)
+    }
+});
+
+// index route
+app.get("/fruits", async (req, res) => {
+    try {
+      const fruits = await Fruit.find({});
+      res.render("fruits/index.ejs", { fruits });
+    } catch (error) {
+      res.status(400).send(error.message);
+    }
+  });
+
+
+
+//////////////////////////////////////////////
+const PORT = process.env.PORT
+app.listen(PORT, () => console.log(`I am alive on port ${PORT}`))
