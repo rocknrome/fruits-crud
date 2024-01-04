b4cc5e74f048511e2a654687a41357e1d12989ee
Roman Larionov <pcsaveddotcom@gmail.com>
2024-01-03 19:59:02 -0500

    seed file with try/catch

--- models/seed.js
+++ models/seed.js
@@ -10,26 +10,29 @@ const Fruit = require("./fruits");
 mongoose.connection.on("open", async () => {
   // seed code goes in this function
 
-  // Run any database queries in this function
-  const startFruits = [
-    { name: "Orange", color: "orange", readyToEat: false },
-    { name: "Grape", color: "purple", readyToEat: false },
-    { name: "Banana", color: "orange", readyToEat: false },
-    { name: "Strawberry", color: "red", readyToEat: false },
-    { name: "Coconut", color: "brown", readyToEat: false },
-  ];
+  try {
+    const startFruits = [
+      { name: "Orange", color: "orange", readyToEat: false },
+      { name: "Grape", color: "purple", readyToEat: false },
+      { name: "Banana", color: "orange", readyToEat: false },
+      { name: "Strawberry", color: "red", readyToEat: false },
+      { name: "Coconut", color: "brown", readyToEat: false },
+    ];
 
-  // Delete all fruits
-  await Fruit.deleteMany({});
+    // Delete all fruits
+    await Fruit.deleteMany({});
 
-  // Seed Starter Fruits
-  const data = await Fruit.create(startFruits);
+    // Seed Starter Fruits
+    const data = await Fruit.create(startFruits);
 
-  // log the create fruits to confirm
-  console.log("--------FRUITS CREATED----------");
-  console.log(data);
-  console.log("--------FRUITS CREATED----------");
+    // log the create fruits to confirm
+    console.log("--------FRUITS CREATED----------");
+    console.log(data);
+    console.log("--------FRUITS CREATED----------");
 
-  // close the DB connection
-  mongoose.connection.close();
+    // close the DB connection
+    mongoose.connection.close();
+  } catch (error) {
+    console.log("-------", error.message, "-----------");
+  }
 });
